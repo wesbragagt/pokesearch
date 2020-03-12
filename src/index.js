258593044fd1db7da40e821cc95ffb9337131b0a
weslley braga <wesbragagt@gmail.com>
2020-03-11 21:23:18 -0500

    getting data from graphql api

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import ApolloClient, { gql } from 'apollo-boost'
+import { ApolloProvider } from '@apollo/react-hooks'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/',
+  cache: new InMemoryCache()
+})
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const GET_POKEMONS = gql`
+    query {
+  getPokemons(search: "s") {
+    Pokemon
+    Type1
+    Type2
+    GIF
+    Description
+  }
+}
+`
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+client.query({ query: GET_POKEMONS }).then(result => console.log(result))
+
+ReactDOM.render(<ApolloProvider client={client}><App /></ApolloProvider>, document.getElementById('root'))
