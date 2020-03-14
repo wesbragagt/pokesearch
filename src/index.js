55c4bb160bdc674666662b72089b1fbcc066cd4b
weslley braga <wesbragagt@gmail.com>
2020-03-13 22:57:05 -0500

    commit fix

--- src/index.js
+++ src/index.js
@@ -2,27 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import ApolloClient, { gql } from 'apollo-boost';
+import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+
 const client = new ApolloClient({
   uri: 'http://localhost:4000/',
   cache: new InMemoryCache()
 });
-
-const GET_POKEMONS = gql`
-  query {
-    getPokemons {
-      Pokemon
-      Type1
-      Type2
-      GIF
-      Description
-    }
-  }
-`;
-
-client.query({ query: GET_POKEMONS }).then(result => console.log(result.data));
 
 ReactDOM.render(
   <ApolloProvider client={client}>
